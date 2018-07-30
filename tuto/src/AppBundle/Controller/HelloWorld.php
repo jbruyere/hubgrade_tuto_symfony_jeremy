8fecb6e87d581fd47c7cd4caeffcb31b96ca59e4
Jérémy BRUYERE <41893866+jbruyere@users.noreply.github.com>
2018-07-30 17:31:16 +0200

    route hello world (corrected)

--- tuto/src/AppBundle/Controller/HelloWorld.php
+++ tuto/src/AppBundle/Controller/HelloWorld.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class HelloWorld
 {
 	/**
 	* @Route("/hello-world")
