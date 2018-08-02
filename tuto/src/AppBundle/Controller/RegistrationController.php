e0f2712201b2c255a7e71f6b20879240e88f1efb
Jérémy BRUYERE <41893866+jbruyere@users.noreply.github.com>
2018-08-02 12:07:53 +0200

    attributs user

--- tuto/src/AppBundle/Controller/RegistrationController.php
+++ tuto/src/AppBundle/Controller/RegistrationController.php
@@ -13,6 +13,8 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Debug\Exception;
+use \DateTime;
+
 
 class RegistrationController extends FOSRestController //Controller
 {
@@ -27,28 +29,14 @@ class RegistrationController extends FOSRestController //Controller
 		$password = $this->get('security.password_encoder')
 				->encodePassword($user, $request->get('password'));
 		$user->setPassword($password);
+		$user->setPseudo($request->get('pseudo'));
+		$user->setInscriptionDate($this->getTime());
 		$form = $this->createForm(UserType::class, $user);
 		$form->submit($request->request->all());
 
-		$repository = $this->getDoctrine()
-			->getManager()->getRepository('AppBundle:User');
-		$list = $repository->findAll();
-		$exist = false;
-		echo '<p>DB : <br /></p>';
-		foreach ($list as $us) {
-			echo '<p> username : '
-			.$us->getUsername().' email :'.$us->getEmail().'</p>';
-			if ($user->getUsername() === null
-				|| $user->getEmail() === null 
-				|| $us->getUsername() == $user->getUsername()
-				|| $us->getEmail() == $user->getEmail()) {
-					$exist = true;
-				}
-		}
-
-		if ($exist == true) {
+		if ($this->checkCorrectUser($user) == false) {
 			$view = $this->view(array(
-			'Error' => 'account not set or already exist.'), 500);
+			'Error' => 'Account not set. Error in data or account already exist.'), 500);
 
 			return $this->handleView($view);
 		}
@@ -67,6 +55,41 @@ class RegistrationController extends FOSRestController //Controller
 		$view = $this->view($data, 200);
 
 		return $this->handleView($view);
+	}
+
+	private function getTime()
+	{
+		$timezone = date_default_timezone_get();
+		date_default_timezone_set($timezone);
+		$datetime = new DateTime();
+		return $datetime;
+	}
+
+	private function checkCorrectUser(User $user)
+	{
+		$repository = $this->getDoctrine()
+			->getManager()->getRepository('AppBundle:User');
+		$list = $repository->findAll();
+
+		echo '<p>DB : <br /></p>';
+		foreach ($list as $us) {
+			echo '<p> username : '
+			.$us->getUsername().' email :'.$us->getEmail().'</p>';
+			if ($user->getUsername() === null
+				|| $user->getEmail() === null 
+				|| $us->getUsername() == $user->getUsername()
+				|| $us->getEmail() == $user->getEmail()) {
+					return false;
+				}
+		}
+
+		if ($user->getEmail() == null
+		|| $user->getPseudo() == null
+		|| $user->getUsername() == null
+		|| $user->getPassword() == null) {
+			return false;
+		}
+		return true;
 	}
 }
 ?>
